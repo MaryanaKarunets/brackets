6bca18195d4b7393caad8d694c0c55fbd6d12974
Maryana Karunets <MaryanaKarunets@MacBook-Air-Maryana.local>
2024-08-12 00:53:11 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    let stack = [];
+    let open_brackets = [];
+    let close_brackets = [];
+
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        open_brackets.push(bracketsConfig[i][0]);
+        close_brackets.push(bracketsConfig[i][1]);
+    }
+
+    for (let y = 0; y < str.length; y++) {
+        if (open_brackets.includes(str[y]) && (close_brackets.includes(str[y]) == false)) {
+            stack.push(str[y]);
+        } else if (open_brackets.includes(str[y]) && (stack.length == 0)) {
+            stack.push(str[y]);
+        } else {
+            for (let k = 0; k < bracketsConfig.length; k++) {
+                if ((stack[stack.length - 1] == bracketsConfig[k][0]) && (str[y] == bracketsConfig[k][1])) {
+                    stack.pop();
+                }
+            }
+        }
+    }
+
+    if (stack.length == 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
